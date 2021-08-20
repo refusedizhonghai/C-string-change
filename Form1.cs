15e64cd3707e5d00d899cc7dcf239d6673030385
refusedizhonghai <1970667835@qq.com>
2021-08-20 13:48:30 +0800

    独立提交暂存区文件到仓库

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 字符转换
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static double liuliangcontrol = -2.5;
+        private static int l;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            liu();
+        }
+        private void liu()
+        {
+            l = Convert.ToInt16(Math.Round(liuliangcontrol, 1) * 10);                           //需要重新写字符转换
+
+            string liuliang = Convert.ToString(l, 16);                                     //需要重新写字符转换
+            
+            string liuliangset = "01 05 00 01 " + liuliang.ToString();
+            textBox1.Text = liuliangset;
+        }
+
+    }
+}
